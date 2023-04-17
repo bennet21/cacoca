b3b091c96a026cd2b57e35f0d08f63c572169063
effingpaul <effingpaul@tu-berlin.de>
2023-04-17 20:12:25 +0200

    added Project_viewer.xlsm

--- src/input/project_viewer_h2share_wrapper.py
+++ src/input/project_viewer_h2share_wrapper.py
@@ -1,9 +1,10 @@
 from read_scenario_data import read_all_scenario_data
 import pandas as pd
+import sys
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('max_colwidth', -1)
-data, h2share = read_all_scenario_data('data/scenarios/isi/')
+data, h2share = read_all_scenario_data(sys.argv[1]) #data/scenarios/isi/
 print(h2share.filter(["Scenario"]))
