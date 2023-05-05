010cc033d36603007359ce24a43949bdaee370c0
Jakob Duerrwaechter <jakob.duerrwaechter@pik-potsdam.de>
2023-05-05 18:46:12 +0200

    comment setup

--- cacoca/setup/setup.py
+++ cacoca/setup/setup.py
@@ -3,8 +3,35 @@ from .select_scenario_data import select_prices, select_free_allocations, select
 
 
 class Setup():
+    """
+    Container for the whole setup of a cacoca run, including a config dictionary and dataframes
+    with all necessary data (description in the constructor);
+    Prices, free allocations and h2share are read in as "raw" and can then be selected/modified by
+    the class methods below.
+    For the projects DF, a version with all projects and one with currently selected ones exists.
+    """
 
     def __init__(self, config_filepath: str = None, config: dict = None):
+
+        # parameter dictionary read in from a yml file
+        self.config = None
+        # df of tranformative projects and their properties/definition
+        self.projects_all = None
+        self.projects_current = None
+        # techno-economic parameters (energy demands, costs, emissions) per technology
+        self.techdata = None
+        # reference technology under EU ETS
+        self.reference_tech = None
+        # time series for co2, energy carrier and feedstock prices
+        self.prices_raw = None
+        self.prices = None
+        # ETS free allocations
+        self.free_allocations_raw = None
+        self.free_allocations = None
+        # time series scenarios for share of 'new' fuel mix;
+        # For steel_dri, this is the h2 share; for cement, the share with CCS
+        self.h2share_raw = None
+        self.h2share = None
 
         if config_filepath is None and config is not None:
             self.config = config
@@ -33,10 +60,18 @@ class Setup():
         return
 
     def select_scenario_data(self, scenarios: dict):
+        """
+        - Select by the scenario names given in the config;
+        - Transform: Calendar years are columns in the raw data and rows in the selected data.
+        """
         if isinstance(scenarios, str):
             scenarios = self.config[scenarios]
         self.prices = select_prices(self.prices_raw, scenarios)
         self.free_allocations = select_free_allocations(self.free_allocations_raw, scenarios)
 
     def select_h2share(self, auction_year: int = None):
+        """
+        - Select by the h2 share scenario names given in the projects df for each project
+        - Transform: Ooperation years are columns in the raw data and rows in the selected data.
+        """
         self.h2share = select_h2share(self.h2share_raw, self.projects_current, auction_year)
