11afec21769acf87565097f9598f5e0e23ca3927
Jakob Duerrwaechter <jakob.duerrwaechter@pik-potsdam.de>
2023-05-19 12:28:04 +0200

    separate plotting price scenarios and h2share scenarios

--- cacoca/output/plot_price_scenarios.py
+++ cacoca/output/plot_price_scenarios.py
@@ -1,6 +1,7 @@
 import plotly as pl
 import pandas as pd
 from ..setup.read_input import read_raw_scenario_data
+from ..setup.setup import Setup
 from ..setup.select_scenario_data import years_to_rows
 from .plot_project_cost_time_curves import plot_project
 from .plot_tools import add_color, show_and_save, set_yrange_min_zero
@@ -8,32 +9,45 @@ from .plot_tools import display_name as dn
 # from tools.sensitivities import sensitivity_to_bounds
 
 
-def plot_price_scenarios(config: dict, projects: pd.DataFrame, project_names_h2share: list,
-                         do_emphasize: bool = True):
+def plot_price_scenarios(setup: Setup, do_emphasize: bool = True):
 
-    print_other = True
+    scen_dir_path = setup.config['scenarios_dir']
 
-    scen_dir_path = config['scenarios_dir']
-
-    prices_raw, _, h2share_raw = read_raw_scenario_data(dirpath=scen_dir_path)
+    prices_raw, _, _ = read_raw_scenario_data(dirpath=scen_dir_path)
 
     prices = years_to_rows(prices_raw, year_name="Period", value_name="Price")
     prices = prices.drop(columns='Source Reference')
 
-    if project_names_h2share is not None:
-        # Hack to make it fit in price plotting scheme below
-        h2share = years_to_rows(h2share_raw, year_name="Period", value_name="Price")
+    plot_scenarios(prices, setup.config, do_emphasize)
 
-        h2share_scens = []
-        for project_name in project_names_h2share:
-            h2share_scen = projects \
-                .query(f"`Project name` == '{project_name}'")["H2 Share Scenario"] \
-                .values[0]
-            h2share_scens.append(h2share_scen)
 
-        h2share["Component"] = "H2 Share"
-        h2share["Unit"] = "H2 Share"
-        prices = pd.concat([prices, h2share])
+def plot_h2share_scenarios(setup: Setup, project_names: list,
+                           do_emphasize: bool = True):
+
+    scen_dir_path = setup.config['scenarios_dir']
+
+    _, _, h2share_raw = read_raw_scenario_data(dirpath=scen_dir_path)
+
+    # Hack to make it fit in price plotting scheme below
+    h2share = years_to_rows(h2share_raw, year_name="Period", value_name="Price")
+
+    h2share_scens = []
+    for project_name in project_names:
+        h2share_scen = setup.projects_all \
+            .query(f"`Project name` == '{project_name}'")["H2 Share Scenario"] \
+            .values[0]
+        h2share_scens.append(h2share_scen)
+
+    h2share["Component"] = "H2 Share"
+    h2share["Unit"] = "H2 Share"
+
+    plot_scenarios(h2share, setup.config, do_emphasize, h2share_scens)
+
+
+def plot_scenarios(prices: pd.DataFrame, config: dict, do_emphasize: bool = True,
+                   highlighted_scenarios: list = None):
+
+    print_other = True
 
     for component_name, cdf in prices.groupby('Component'):
 
@@ -51,8 +65,8 @@ def plot_price_scenarios(config: dict, projects: pd.DataFrame, project_names_h2s
             if not do_emphasize:
                 emphasize = 'all_equal'
             else:
-                if component_name == 'H2 Share':
-                    is_emphasized = scenario_name in h2share_scens
+                if highlighted_scenarios:
+                    is_emphasized = scenario_name in highlighted_scenarios
                 else:
                     is_emphasized = \
                         config['scenarios_actual']['prices'][component_name] == scenario_name
