a65db788ac3b0ef9b609ca628fa4489bfb22aea7
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-12-15 17:55:01 +0100

    import posted data to cacoca, adjust project and slide plotting

--- plot_slides_posted.py
+++ plot_slides_posted.py
@@ -10,12 +10,14 @@ if 'cost_and_em_actual' not in globals():
     setup, cost_and_em_actual = run(config_filepath='config/config_posted.yml')
     change_output_subdir_by_filename(setup.config, __file__)
 
-project_names = [
-    'Zement Posted'
-]
+project_names = setup.projects_current['Project name'].unique().tolist()
+
 for project_name in project_names:
-    plot_stacked_bars(cost_and_em_actual, setup.config, project_name=project_name,
-                      cost_per='product')
+    try:
+        plot_stacked_bars(cost_and_em_actual, setup.config, project_name=project_name,
+                        cost_per='product')
+    except Exception as e:
+        print(f"Could not plot {project_name}: {e}")
     # plot_stacked_bars(cost_and_em_actual, config, project_name=project_name,
     #                   cost_per='em_savings', is_diff=True)
 
